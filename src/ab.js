079701e82f3d1129576abc3214876a079fff5c12
Anders Mattson <herr.mattson@gmail.com>
2010-09-23 14:34:41 +0200

    First commit

---
+++ src/ab.js
@@ -0,0 +1,46 @@
+var AB = window.AB = AB || {
+	param: 'ab_js=',
+	link: function(){
+		
+		if(arguments.length < 2)
+			return;
+		
+		var e = typeof arguments[0] == "string" ? document.getElementById(arguments[0]) : e;
+		
+		if(!e)
+			return;
+
+		var o, p, P, j, h, i = Math.floor((arguments.length)*Math.random());
+	
+		o = i == 0 ? {} : arguments[i];
+			
+		if(o.html)
+			e.innerHTML = o.html;
+		
+		P = o.param || AB.param;
+		
+		if(o.style) {
+			if(typeof o.style == "object")
+				for(j in o.style)
+					e.style[j] = o.style[j];
+			else
+				e.setAttribute('style', o.style);
+		}
+		
+		h = e.href;
+		if(h) {
+			if(o.href)
+				h = o.href[0] == '+' ? h + o.href.substring(1) : o.href;
+			else {
+				if(h.indexOf('#') > -1) {
+					p = h.split("#");
+					h = p[0] + ((h.indexOf('?') > -1) ? '&' : '?') + P + i + '#' + p[1];
+				}
+				else
+					h = h + ((h.indexOf('?') > -1) ? '&' : '?') + P + i;
+			}
+	
+			e.href = h;
+		}
+	}
+};
