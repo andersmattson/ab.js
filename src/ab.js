41ec0e6085193d7ba2846920bac57baa5719e991
Anders Mattson <herr.mattson@gmail.com>
2011-04-07 07:13:40 -0700

    Added support for current url validation and variation callbacks.

--- src/ab.js
+++ src/ab.js
@@ -1,17 +1,38 @@
 /*
- *	ab.js JavaScript A/B-testing tool v0.2
+ *	ab.js JavaScript A/B-testing tool v0.3
  *	
  *	Copyright 2010, Anders Mattson
  *	Licensed under the MIT or GPL Version 2 licenses.
  *	
+ *  AB.vary( Settings (Object), Variation #1 (Object) [, Variation #2 (Object) [, ...]] )
+ *  
+ *  AB.vary({
+ *  		name: 'unique_query_parameter_name',
+ *  		elem: 'id_string_or_DOM_Node',
+ *  		url: 'http://validation.url.for/variation'
+ *  	},{
+ *  		html: 'html_content',
+ *  		style: {
+ *  			'css-attribute': 'css-attribute-value',
+ *  			...
+ *  		},
+ *  		fn: function(){...},
+ *  		href: 'http://variation.url' (starting with a plus appends the string to the urls)
+ *  	},{
+ *  		...
+ *  });
  */
 
 var AB = window.AB = {
+
 	vary: function(d){
+
 		var j = d.elem || d,
 			e = typeof j == "string" ? document.getElementById(j) : j;
 		
+		// Applying the variation to multiple DOM elements
 		if(!e || e.constructor == Array) {
+
 			for(j in e) {
 				if(!d.elem)
 					d = {};
@@ -19,8 +40,15 @@ var AB = window.AB = {
 				arguments[0] = d;
 				AB.vary.apply(this, arguments);
 			}
+
 			return;
+
 		}
+		
+		// Check if the variation is for a specific url and if so, check that it matches the current one.
+		if(d.url && !AB.compareUrls(d.url, document.location.href))
+			return;
+		
 		
 		var o, p, c, h = document.location.href, A = arguments, P = (d.name || 'ab_js'), 
 			i = (c = document.cookie.match(new RegExp("(?:^|[^a-z0-9_\-]+)" + P + "=([0-9]+)", "i"))) ? 
@@ -40,11 +68,11 @@ var AB = window.AB = {
 			for(j in o.style)
 				e.style[j] = o.style[j];
 		
-		// Last we replace all the links found in the changed document.
+		// Last we replace all the links found in the changed document fragment.
 		A = e.nodeName == 'A' ? [e] : e.getElementsByTagName('A');
 		
 		for(var j in A) {
-			h = A[j].href;
+			h = A[j].getAttribute('href');
 			if(h) {
 				if(o.href)
 					h = o.href[0] == '+' ? h + o.href.substring(1) : o.href;
@@ -54,10 +82,21 @@ var AB = window.AB = {
 						h = p[0] + ((h.indexOf('?') > -1) ? '&' : '?') + P + '=' + i + '#' + p[1];
 					}
 					else
-						h = h + ((h.indexOf('?') > -1) ? '&' : '?') + P + '=' + i;
+						h += ((h.indexOf('?') > -1) ? '&' : '?') + P + '=' + i;
 				}
-				A[j].href = h;
+				A[j].setAttribute('href', h);
 			}
 		}
+		
+		if(o.fn)
+			fn.apply(this, arguments);
+	},
+	
+	// Checks if two urls points to the same source, regardless of the order between the parameters.
+	// The hash tag is ignored.
+	compareUrls: function(a, b) {
+		var as, bs;
+		return a == b || ((as = a.split('#')[0].split('?')).length > 1 ? as[0] + '?' + as[1].split('&').sort().join('&') : as[0]) == 
+			   ((bs = b.split('#')[0].split('?')).length > 1 ? bs[0] + '?' + bs[1].split('&').sort().join('&') : bs[0]);
 	}
 };
